3cdf77a4992acb7368019cadf74a34c1043d9474
BlanahrAbarstyrn <ljspromo@protonmail.com>
2025-04-17 13:24:16 -0400

    CH2:Player, L4:Sprites - circleshape.py created

---
+++ circleshape.py
@@ -0,0 +1,26 @@
+# Throughout this project, some code was provided by Boot, like the class below.
+# The learning focus is on specific parts and Boot has pre-written the stuff that
+# isn't related to the concepts Boot is trying to teach.
+
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
